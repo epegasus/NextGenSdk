5dbce14f030b1459efd2ef4a4cbdcccda150234b
epegasus <sohaibahmed.hypersoft@gmail.com>
2025-12-01 16:15:14 +0500

    Initial Commit - Native working

--- app/src/main/java/dev/pegasus/nextgensdk/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/dev/pegasus/nextgensdk/ui/fragments/DashboardFragment.kt
@@ -1,22 +1,12 @@
 package dev.pegasus.nextgensdk.ui.fragments
 
-import android.view.LayoutInflater
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
-import com.google.android.libraries.ads.mobile.sdk.nativead.MediaView
-import com.google.android.libraries.ads.mobile.sdk.nativead.NativeAd
-import com.google.android.libraries.ads.mobile.sdk.nativead.NativeAdView
 import dev.pegasus.nextgensdk.R
 import dev.pegasus.nextgensdk.databinding.FragmentDashboardBinding
-import dev.pegasus.nextgensdk.databinding.NativeAdViewBinding
 import dev.pegasus.nextgensdk.inter.callbacks.InterstitialOnShowCallBack
 import dev.pegasus.nextgensdk.inter.enums.InterAdKey
-import dev.pegasus.nextgensdk.nativeads.callbacks.NativeOnShowCallback
 import dev.pegasus.nextgensdk.nativeads.enums.NativeAdKey
 import dev.pegasus.nextgensdk.utils.base.fragment.BaseFragment
 
@@ -65,35 +55,8 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>(FragmentDashboa
     private fun showNativeAd() {
         diComponent.nativeAdsConfig.pollNativeAd(key = NativeAdKey.DASHBOARD, showCallback = null)?.let {
             if (isAdded.not()) return
-            bindNativeAdToContainer(it, binding.flNative)
+            binding.nativeAdView.setNativeAd(it)
         }
-    }
-
-    private fun bindNativeAdToContainer(nativeAd: NativeAd, container: FrameLayout) {
-        val nativeAdBinding = NativeAdViewBinding.inflate(layoutInflater)
-        container.removeAllViews()
-        container.addView(nativeAdBinding.root)
-
-        // Set the native ad view elements.
-        val nativeAdView = nativeAdBinding.root
-        nativeAdView.advertiserView = nativeAdBinding.adAttribute
-        nativeAdView.bodyView = nativeAdBinding.adBody
-        nativeAdView.callToActionView = nativeAdBinding.adCallToAction
-        nativeAdView.headlineView = nativeAdBinding.adHeadline
-        nativeAdView.iconView = nativeAdBinding.adAppIcon
-
-        // Set the view element with the native ad assets.
-        nativeAdBinding.adAttribute.text = nativeAd.advertiser
-        nativeAdBinding.adBody.text = nativeAd.body
-        nativeAdBinding.adCallToAction.text = nativeAd.callToAction
-        nativeAdBinding.adHeadline.text = nativeAd.headline
-        nativeAdBinding.adAppIcon.setImageDrawable(nativeAd.icon?.drawable)
-
-        // Hide views for assets that don't have data.
-        nativeAdBinding.adAppIcon.isVisible = nativeAd.icon != null
-
-        // Inform the Google Mobile Ads SDK that you have finished populating the native ad views with this native ad.
-        nativeAdView.registerNativeAd(nativeAd, nativeAdBinding.adMediaView)
     }
 
     private fun checkInterstitialAd(caseType: Int) {
